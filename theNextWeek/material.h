eb762ea23c90313aed3534f37ed3799134104fce
John D. <John.LouisDiP@gmail.com>
2026-08-21 00:07:20 -0500

    Add files via upload

--- theNextWeek/material.h
+++ theNextWeek/material.h
@@ -28,7 +28,7 @@ public:
 			scatter_direction = rec.normal;
 		}
 		
-		scattered = ray(rec.p, scatter_direction);
+		scattered = ray(rec.p, scatter_direction, r_in.time());
 		attenuation = albedo;
 		return true;
 	}
@@ -44,8 +44,9 @@ public:
 	const override {
 		vec3 reflected = reflect(r_in.direction(), rec.normal);
 		reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected, r_in.time());
 		attenuation = albedo;
+		
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
 private:
@@ -76,7 +77,7 @@ public:
 			direction = refract(unit_direction, rec.normal, ri);
 		}
 		
-		scattered = ray(rec.p, direction);
+		scattered = ray(rec.p, direction, r_in.time());
 		return true;
 	}
 
